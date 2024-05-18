e65aaa230a4f743c8e327bdc9bac93d33fa6f597
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-05-18 23:05:57 +0530

    Update Navigation.jsx

--- src/customers/components/Navigation/Navigation.jsx
+++ src/customers/components/Navigation/Navigation.jsx
@@ -18,6 +18,7 @@ import TextField from "@mui/material/TextField";
 import AuthModel from "../../Auth/AuthModel";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser, logout } from "../../../States/Auth/Action";
+import healthBuddy from "./healthBuddy.png";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -268,9 +269,9 @@ export default function Navigation() {
 
                   <span className="sr-only">Your Company</span>
                   <img
-                    src="healthBuddy.png"
+                    src={healthBuddy}
                     alt="HealthBuddy"
-                    className="h-8 w-8 mr-2"
+                    className="h-12 w-12 mr-2"
                   />
 
               </div>
