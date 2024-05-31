5a6ecd8f398aa92f10316c9f274ec152a49d344a
Gagan <100960759+Gagan47raj@users.noreply.github.com>
2024-05-31 19:05:06 +0530

    Update InfoCard.jsx

--- src/Admin/components/InfoCard.jsx
+++ src/Admin/components/InfoCard.jsx
@@ -1,16 +1,17 @@
+import { Card } from '@mui/material'
 import React from 'react'
-import { Card, CardBody } from '@windmill/react-ui'
+
 
 function InfoCard({ title, value, children: icon }) {
   return (
     <Card>
-      <CardBody className="flex items-center">
+      <div className="flex items-center space-x-4 p-4">
         {icon}
         <div>
           <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">{title}</p>
           <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">{value}</p>
         </div>
-      </CardBody>
+      </div>
     </Card>
   )
 }
